3b51f004338012d8482af59e3aea447fae2af9ed
aurelioklv <aurelioklv@gmail.com>
2024-03-02 12:18:57 +0700

    feat: add about page

--- app/src/main/java/com/aurelioklv/katalog/AboutActivity.kt
+++ app/src/main/java/com/aurelioklv/katalog/AboutActivity.kt
@@ -1,11 +1,21 @@
 package com.aurelioklv.katalog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val tvEmail: TextView = findViewById(R.id.tv_user_email)
+        tvEmail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:${tvEmail.text}")
+            startActivity(intent)
+        }
     }
 }
